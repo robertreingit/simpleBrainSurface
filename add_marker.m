c109adb96bdd4a547eec08fd7ba93930a4c52246
robertrein <git@robertrein.de>
2015-01-16 23:48:32 +0100

    Added color parameter.

--- add_marker.m
+++ add_marker.m
@@ -1,18 +1,27 @@
-function h = add_marker( mni )
+function h = add_marker( mni, color )
 %ADD_MARKER Summary of this function goes here
 %   Detailed explanation goes here
 % INPUT:
+% mni = vector (1x3) or matrix (nx3) of mni coordinate sets
+% color = marker color
 % OUTPUT:
+% h = handles to marker patches
 % SIDEEFFECTS:
 % Markers are added to the current plot
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+    if (nargin == 1)
+        color = [0.9 0.1 0.1];
+    end
+    
     if isvector(mni)
         if iscolumn(mni)
             mni = mni';
         end
     end
     assert(size(mni,2)==3,'Coordinates must be 3D vectors');
+    
+    
     no_marker = size(mni,1);
     h = zeros(no_marker,1);
     
@@ -20,7 +29,7 @@ function h = add_marker( mni )
         marker = generate_sphere(mni(m,:));
         h(m) = patch('vertices',marker.vertices,...
                   'faces',marker.faces,...
-                  'facecolor',[0.9 0 0],...
+                  'facecolor',color,...
                   'facealpha',0.6,...
                   'edgecolor','none',...
                   'facelighting','gouraud');
