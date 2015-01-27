6bdb6e3a564d96c6cdbd41645f35b84cb80505cc
robertrein <git@robertrein.de>
2015-01-27 22:52:38 +0100

    Changed draw_signal y_axis limits.

--- draw_signal.m
+++ draw_signal.m
@@ -2,8 +2,8 @@ function h_nirs = draw_signal(specs,h)
 %draw_signal
 %
 % INPUT:
-% specs
-% h
+% specs = specs struct
+% h = handle to axes object
 % OUTPUT:
 % h_nirs = handle to nirs data
 % SIDEEFFECTS:
@@ -12,6 +12,10 @@ function h_nirs = draw_signal(specs,h)
 
 set(gcf,'currentaxes',h)
 h_nirs = zeros(1,2);
+y_min = min(min(specs.oxy),min(specs.deoxy));
+y_max = max(max(specs.oxy),max(specs.deoxy));
+d_y = y_max - y_min;
+y_limits_factor = 0.1;
 h_nirs(1) = plot(specs.t,specs.oxy,'r');
 set(h,'nextplot','add');
 h_nirs(2) = plot(specs.t,specs.deoxy,'b');
@@ -20,7 +24,8 @@ set(h,'nextplot','replace',...
     'linewidth',1,...
     'xcolor','white',...
     'ycolor','white',...
-    'xlim',specs.t([1,end]));
+    'xlim',specs.t([1,end]),...
+    'ylim',[y_min-y_limits_factor*d_y,y_max+y_limits_factor*d_y]);
 set(h_nirs,'linewidth',2);
 xlabel('Time','color','white')
 ylabel('Response','color','white');
